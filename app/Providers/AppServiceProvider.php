8b23eb365d6ff7a832e670462421e4f8367fc5d1
Johan Jareth Ochoa Rosales <joan6jareth55@gmail.com>
2026-07-06 23:39:42 -0600

    Feat: Configura HTTPS con certificados SSL en proxy Nginx

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,16 +3,21 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-   public function boot(): void
+    public function boot(): void
     {
-        // Si existe _FILE, leer el secreto desde el archivo
+        // 1. Forzar el esquema HTTPS
+        URL::forceScheme('https');
+
+        // 2. Si existe _FILE, leer el secreto desde el archivo
         foreach ($_ENV as $key => $value) {
             if (str_ends_with($key, '_FILE') && file_exists($value)) {
                 $realKey = str_replace('_FILE', '', $key);
                 $secret  = trim(file_get_contents($value));
+                
                 config([strtolower(str_replace('_', '.', $realKey)) => $secret]);
                 putenv("$realKey=$secret");
             }
